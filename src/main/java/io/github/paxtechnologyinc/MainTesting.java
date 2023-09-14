0223fbf54c49d98e9992285dedbbc357ff7da4be
Ruize_Ren_Pax <ruize.ren@pax.us>
2023-09-14 15:56:20 -0400

    accomplish publish and release to maven central

--- src/main/java/io/github/paxtechnologyinc/MainTesting.java
+++ src/main/java/io/github/paxtechnologyinc/MainTesting.java
@@ -2,7 +2,7 @@ package io.github.paxtechnologyinc;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class MainTesting {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
@@ -15,5 +15,8 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+    }
+    public static void print(){
+        System.out.println("BalabLaba");
     }
 }
